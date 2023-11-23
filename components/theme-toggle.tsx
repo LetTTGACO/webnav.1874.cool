93cd58238c5fa8103f5f24397c7f9ebb4556187b
LetTTGACO <1225751694@qq.com>
2023-11-23 10:47:41 +0800

    feat: init

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -12,7 +12,7 @@ export function ThemeToggle() {
   return (
     <Button
       variant="ghost"
-      size="icon"
+      size="sm"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
       <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
