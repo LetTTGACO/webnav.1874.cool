55c8ff90ac5a5f5f040efd596defe25654319116
LetTTGACO <1225751694@qq.com>
2023-12-03 23:29:50 +0800

    feat: config

--- app/links.ts
+++ app/links.ts
@@ -1,30 +1,30 @@
 import type { Prisma } from "@prisma/client"
 
-import prisma from "@/lib/db"
+// import prisma from "@/lib/db"
 import { mockList } from "@/lib/mockList"
 
 export default async function getNavLinks() {
-  const res = await prisma.category.findMany({
-    orderBy: [
-      {
-        rank: "asc",
-      },
-    ],
-    include: {
-      links: {
-        orderBy: {
-          rank: "asc",
-        },
-        where: {
-          public: true,
-          status: 1,
-        },
-      },
-    },
-  })
-  // console.log(res)
+  // const res = await prisma.category.findMany({
+  //   orderBy: [
+  //     {
+  //       rank: "asc",
+  //     },
+  //   ],
+  //   include: {
+  //     links: {
+  //       orderBy: {
+  //         rank: "asc",
+  //       },
+  //       where: {
+  //         public: true,
+  //         status: 1,
+  //       },
+  //     },
+  //   },
+  // })
+  // // console.log(res)
   // console.log(mockList)
-  return res
+  return mockList
 }
 
 export type CategoryWithLinks = Prisma.PromiseReturnType<typeof getNavLinks>
